c9f88e4587c5aaced5e9d33cce3f1e4f94918983
Andika Eka Kurnia <dikdnssocial@gmail.com>
2021-07-29 11:54:30 +0700

    add reset button

---
+++ script/script.js
@@ -0,0 +1,64 @@
+/**
+ * To display the game status
+ */
+const gameStatus = document.querySelector('#status');
+
+/**
+ * To check the game progress
+ */
+let gameIsActive = true;
+
+/**
+ * Player Variable
+ */
+let currentPlayer = 'DikDns';
+
+/**
+ * To store game state data here
+ */
+let gameState = ["","","","","","","","",""];
+
+
+/**
+ * Status Messages Functions
+ */
+const winStatus = () => '<div class="status-messages" id="win"><p>Player ' + currentPlayer + ' Win!</p></div>';
+const drawStatus = () => '<div class="status-messages" id="draw"><p>Draw!</p></div>';
+const currentPlayerTurn = () => '<div class="status-messages" id="current"><p>It\'s ' + currentPlayer + '\'s turn.</p></div>';
+
+
+/**
+ * Initial Messages
+ */
+gameStatus.innerHTML = currentPlayerTurn();
+
+
+/**
+ * main function list
+ */
+function handleCellPlayed() {
+    
+}
+
+function handlePlayerChange() {
+    
+}
+
+function handleResultValidation() {
+    
+}
+
+function handleCellClicked() {
+    
+}
+
+function handleRestartGame() {
+    
+}
+
+
+
+/**
+ * Select the cell and the restart button
+ */
+
