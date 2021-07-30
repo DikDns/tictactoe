fad98e4ced784077e5c8b71f698c0a178cc02919
Andika Eka Kurnia <dikdnssocial@gmail.com>
2021-07-30 11:27:01 +0700

    finish result validator and cell played function

--- script/script.js
+++ script/script.js
@@ -18,6 +18,19 @@ let currentPlayer = 'X';
  */
 let gameState = ["","","","","","","","",""];
 
+/**
+ * Winning Condition Array
+ */
+const winningConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 
 /**
  * Status Messages Functions
@@ -36,8 +49,12 @@ gameStatus.innerHTML = currentPlayerTurn();
 /**
  * main function list
  */
-function handleCellPlayed() {
-    
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    // Update the internal state and UI state
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+
+    console.log(gameState);
 };
 
 function handlePlayerChange() {
@@ -45,7 +62,46 @@ function handlePlayerChange() {
 };
 
 function handleResultValidation() {
-    
+    // Set won boolean
+    let roundWon = false;
+
+    // Iterate into all the winningConditions Array
+    for (let i = 0; i <= 7; i++){
+        // Get the wincondition sub array
+        const winCondition = winningConditions[i];
+        // Put the wincondition coordinate into the gameState array number
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        
+        // Skip the loop if one of the cell is not the correct combinations
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+
+        // Break the loop if the cell is correct combination and change the won boolean value
+        if (a === b && b === c){
+            roundWon = true;
+            break;
+        }
+    }
+
+    // If the winner in this round
+    if (roundWon){
+        gameStatus.innerHTML = winStatus();
+        gameIsActive = false;
+        return;
+    }
+
+    // If this round tie
+    if (!gameState.includes("")){
+        gameStatus.innerHTML = drawStatus();
+        gameIsActive= false;
+        return;
+    }
+
+    // Change Turn
+    handlePlayerChange();
 };
 
 function handleCellClicked(clickedCellEvent) {
@@ -55,10 +111,9 @@ function handleCellClicked(clickedCellEvent) {
     const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
 
     // Check if the cell is already clicked or game paused
-    if (gameStatus[clickedCellIndex] !== "" || !gameIsActive) {
+    if (gameState[clickedCellIndex] !== "" || !gameIsActive) {
         return;
     }
-    console.log(clickedCellIndex);
 
 
     // Continue to the next step function
