6a88814c34d9a260f4920c2c30ef33cd740496db
Andika Eka Kurnia <dikdnssocial@gmail.com>
2021-07-30 11:43:19 +0700

    finish reset handler function

--- script/script.js
+++ script/script.js
@@ -50,7 +50,7 @@ gameStatus.innerHTML = currentPlayerTurn();
  * main function list
  */
 function handleCellPlayed(clickedCell, clickedCellIndex) {
-    // Update the internal state and UI state
+    //* Update the internal state and UI state
     gameState[clickedCellIndex] = currentPlayer;
     clickedCell.innerHTML = currentPlayer;
 
@@ -58,73 +58,80 @@ function handleCellPlayed(clickedCell, clickedCellIndex) {
 };
 
 function handlePlayerChange() {
-    // Swap the turn using ternary operator awesome!
+    //* Swap the turn using ternary operator awesome!
     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
     gameStatus.innerHTML = currentPlayerTurn();
 };
 
 function handleResultValidation() {
-    // Set won boolean
+    //* Set won boolean
     let roundWon = false;
 
-    // Iterate into all the winningConditions Array
+    //* Iterate into all the winningConditions Array
     for (let i = 0; i <= 7; i++){
-        // Get the wincondition sub array
+        //* Get the wincondition sub array
         const winCondition = winningConditions[i];
-        // Put the wincondition coordinate into the gameState array number
+        //* Put the wincondition coordinate into the gameState array number
         let a = gameState[winCondition[0]];
         let b = gameState[winCondition[1]];
         let c = gameState[winCondition[2]];
         
-        // Skip the loop if one of the cell is not the correct combinations
+        //* Skip the loop if one of the cell is not the correct combinations
         if (a === '' || b === '' || c === '') {
             continue;
         }
 
-        // Break the loop if the cell is correct combination and change the won boolean value
+        //* Break the loop if the cell is correct combination and change the won boolean value
         if (a === b && b === c){
             roundWon = true;
             break;
         }
     }
 
-    // If the winner in this round
+    //* If the winner in this round
     if (roundWon){
         gameStatus.innerHTML = winStatus();
         gameIsActive = false;
         return;
     }
 
-    // If this round tie
+    //* If this round tie
     if (!gameState.includes("")){
         gameStatus.innerHTML = drawStatus();
         gameIsActive= false;
         return;
     }
 
-    // Change Turn
+    //* Change Turn
     handlePlayerChange();
 };
 
 function handleCellClicked(clickedCellEvent) {
-    // Select the clicked cell
+    //* Select the clicked cell
     const clickedCell = clickedCellEvent.target;
-    // Select the data-cell-index
+    //* Select the data-cell-index
     const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
 
-    // Check if the cell is already clicked or game paused
+    //* Check if the cell is already clicked or game paused
     if (gameState[clickedCellIndex] !== "" || !gameIsActive) {
         return;
     }
 
 
-    // Continue to the next step function
+    //* Continue to the next step function
     handleCellPlayed(clickedCell, clickedCellIndex);
     handleResultValidation();
 };
 
 function handleRestartGame() {
+    //* Overwrite the global variable
+    gameIsActive = true;
+    currentPlayer = 'X';
+    gameStatus.innerHTML = currentPlayerTurn();
+    gameState = ["","","","","","","","",""];
 
+    //* Change the cell condition to normal 
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = '');
 };
 
 
